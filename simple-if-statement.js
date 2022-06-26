61830cf2e90a5cda35f66ec60c5076d5cac2e18e
rajea-bilal <rajea.bilal@gmail.com>
2022-06-26 09:37:03 +0100

    simple-if-statment

---
+++ simple-if-statement.js
@@ -0,0 +1,22 @@
+// Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
+
+// To make this challange harder and to promp the challenger to read up about coercion I have disabled some of the built in methods including .toString(), .join(), .split(), parseInt and .Number().
+
+//  Test.assertEquals(add('1', 1), true);
+//  Test.assertEquals(add(1, '1'), true);
+//  Test.assertEquals(add(1, '0'), false);
+//  Test.assertEquals(add('11', 11), true);
+//  Test.assertEquals(add(12, 12), true);
+//  Test.assertEquals(add(120, '021'), false);
+
+function add(a, b){
+  
+    if(a == b) {
+      return true
+    } else if (a !== b) {
+      return false
+    }
+  
+  }
+  
+  console.log(add(120, '021'));
