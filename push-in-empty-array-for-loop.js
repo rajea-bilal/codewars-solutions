659b37d1f22ea92aa3ccac4f42cdcda66613af11
rajea-bilal <rajea.bilal@gmail.com>
2022-07-02 12:53:20 +0100

    this is the commit message

---
+++ push-in-empty-array-for-loop.js
@@ -0,0 +1,26 @@
+// What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
+// Example(Input --> Output)
+
+// "apple ban" --> ["apple 5", "ban 3"]
+// "you will win" -->["you 3", "will 4", "win 3"]
+// Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
+
+// Note: String will have at least one element; words will always be separated by a space.
+
+// Test.assertSimilar(addLength('apple ban'),["apple 5", "ban 3"]);
+// Test.assertSimilar(addLength('you will win'),["you 3", "will 4", "win 3"]);
+
+function addLength(str) {
+  
+    let change = str.split(' ')
+   
+    let number = []
+    for(let i = 0; i < change.length; i++) {
+     number.push(change[i] + ' ' + change[i].length)
+    }
+   return number
+  }
+  
+  
+  console.log(addLength('you will win'));
