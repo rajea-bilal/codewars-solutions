6d7cd91e7cf58b86dfc30faacb16b441e54edbc9
rajea-bilal <rajea.bilal@gmail.com>
2022-07-22 18:01:17 +0100

    this is the commit message

---
+++ kyu-7/return-array-last-two-biggest-numbers.js
@@ -0,0 +1,28 @@
+// The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age,  oldest age].
+
+// The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
+
+// For example (Input --> Output):
+
+// [1, 2, 10, 8] --> [8, 10]
+// [1, 5, 87, 45, 8, 8] --> [45, 87]
+// [1, 3, 10, 0]) --> [3, 10]
+
+function twoOldestAges(ages){
+  
+    let newArray = []
+    let highestNumber = Math.max(...ages)
+    
+    let removeHighest = ages.indexOf(highestNumber)
+    let removal = ages.splice(removeHighest, 1)
+  
+    let secondHighest = Math.max(...ages)
+    newArray.push(secondHighest, highestNumber)
+   
+    return newArray
+  
+  }
+  
+  console.log(twoOldestAges([1, 3, 10, 0]))
+  
+  
