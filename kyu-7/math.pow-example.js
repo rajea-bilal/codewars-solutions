74b6fe285080af107e7253a85b94f723809fa15b
rajea-bilal <rajea.bilal@gmail.com>
2023-01-23 21:36:34 +0000

    math.pow example

---
+++ kyu-7/math.pow-example.js
@@ -0,0 +1,9 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+function square (number) {
+    return Math.pow(number, 2)
+}
+
+console.log(square(4))
+ // assert.strictEqual(square(3), 9);
