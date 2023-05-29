31b72097d8125c5c10265470ba852aeb1ab38e5e
rajea-bilal <rajea.bilal@gmail.com>
2023-05-29 21:50:13 +0100

    figured the difference between ages

---
+++ kyu-7/dads-twice-age.js
@@ -0,0 +1,9 @@
+   const sonsTwice = sonYearsOld * 2
+
+   if(sonsTwice < dadYearsOld) {
+     return dadYearsOld - sonsTwice
+   } else {
+     const difference = sonsTwice - dadYearsOld
+    
+     return difference
+   }
